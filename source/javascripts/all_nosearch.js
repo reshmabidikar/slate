4fdd16b42cf4ffb05749c4d8251d5f64cf4c5f34
MaksymMV <maslakovmaksym243@gmail.com>
2023-08-23 16:43:17 +0300

    Fix table, preloader, copied tolltip, remove hover effect from search

--- source/javascripts/all_nosearch.js
+++ source/javascripts/all_nosearch.js
@@ -1,8 +1,1 @@
 //= require ./lib/_energize
-//= require ./app/_copy
-//= require ./app/_toc
-//= require ./app/_lang
-
-// $(function() {
-//   window.setupLanguages($('body').data('languages'));
-// });
